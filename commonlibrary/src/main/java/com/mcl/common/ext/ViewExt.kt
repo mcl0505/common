f19e56bae2d20b771908c126a5b16eef4d88fe1f
mcl0505 <1655118132@qq.com>
2022-03-19 17:31:50 +0800

    优化点击事件响应

--- commonlibrary/src/main/java/com/mcl/common/ext/ViewExt.kt
+++ commonlibrary/src/main/java/com/mcl/common/ext/ViewExt.kt
@@ -1,6 +1,8 @@
 package com.mcl.common.ext
 
+import android.os.SystemClock
 import android.view.View
+import com.mcl.common.R
 
 /**
  * 公司：坤创科技
@@ -9,17 +11,17 @@ import android.view.View
  * 功能描述：
  */
 
-private var lastClickTime: Long = 0
-//防止快速点击造成打开多个界面   只允许在 1秒内只能点击一次  single(2000){}   可自定义时间
 /**
  * 防止快速点击造成打开多个界面
  */
 fun <T : View> T.singleClick(time: Int = 500, block: (T) -> Unit) {
     this.setOnClickListener {
-        val curClickTime = System.currentTimeMillis()
+
+        val curClickTime = SystemClock.uptimeMillis()
+        val lastClickTime = (it.getTag(R.id.singleClickId) as Long) ?: 0
         if (curClickTime - lastClickTime >= time) {
             // 超过点击间隔后再将lastClickTime重置为当前点击时间
-            lastClickTime = curClickTime
+            it.setTag(R.id.singleClickId, curClickTime)
             block(it as T)
         }
     }
